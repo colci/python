5d48f1d4d83832ab9f5a99291faf8544a0b6105e
Alexander <colci@yandex.ru>
2020-10-17 23:17:35 +0300

    Домашка 3 урока

--- domashka3/zadanie1.py
+++ domashka3/zadanie1.py
@@ -1,4 +1,22 @@
-def setInfoUser(**kwargs):
-    return kwargs
+def divisionNumber(x, y):
+    while True:
+        try:
+            return x / y
+        except ZeroDivisionError:
+            print("Ошибка! нельзя делать на 0")
 
-print(setInfoUser(name = "Иван", surname = "Иванов", year = 1984, city = "Москва", email = "12@mail.ru", phone = "8(954) 255-55-55")
+
+def MenuInput():
+    message = ("Введите первое число: ", "Введите второе число: ")
+    nums = []
+    i = 0
+    while i < 2:
+        try:
+            nums.insert(i, float(input(message[i])))
+            i += 1
+        except ValueError:
+            print("Ошибка вы ввели не число")
+    print(divisionNumber(float(nums[0]), float(nums[1])))
+
+
+MenuInput()
