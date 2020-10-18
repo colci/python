b4a6e58938a1a87cf406dc51b702bb8fd46ab5a4
Alexander <colci@yandex.ru>
2020-10-18 20:59:54 +0300

    1-е задание

---
+++ domashka4/zadanie1.py
@@ -0,0 +1,7 @@
+from sys import argv
+def payroll_accounting(hours_worked, cost_hour, bonus):
+    print(f"Заработная плата сотрудника: {(hours_worked * cost_hour) + bonus}")
+
+script_name, hours_worked, cost_hour, bonus = argv
+if hours_worked.isalnum() and cost_hour.isalnum() and bonus.isalnum():
+    payroll_accounting(int(hours_worked), float(cost_hour), float(bonus))
